395b81fdc77bc320a16fa9d260428cdf397bc941
mayurhapani <hapanimayur@gmail.com>
2024-09-07 15:02:27 +0530

    async handler created

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-import connectDB from "./db/db.js";
-import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./db/db.js";
+import { app } from "./app.js";
 
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("./public"));
-app.use(dotenv());
+dotenv.config({ path: "./env" });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (err) => {
+      console.log("ERROR -on", err);
+      throw err;
+    });
+
+    app.listen(process.env.PORT || 8001, () =>
+      console.log(`server is started on http://localhost:${process.env.PORT}`)
+    );
+  })
+  .catch((err) => console.log(("mongo DB connection failed !!!", err.message)));
