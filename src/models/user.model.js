5e9b4ea497c22f935376e231d9ffc80db9f6ab96
mayurhapani <hapanimayur@gmail.com>
2024-09-09 18:53:47 +0530

    pre added in user

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
   {
@@ -47,6 +49,11 @@ const userSchema = new Schema(
   { timestamps: true }
 );
 
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
 
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 
 export const userModel = mongoose.model("User", userSchema);
